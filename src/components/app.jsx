f6be734c0647a7086d03a1dcf0b9bf2f9fde00f9
morganfula <morgan.fula@me.com>
2020-09-13 19:43:37 +0200

    done

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,47 @@
-/* eslint-disable no-useless-constructor */
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-unused-vars */
+/* eslint-disable react/sort-comp */
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import flats from '../../data/flats';
+import FlatList from './flat_list';
+import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      selectedFlats: flats[0],
+      selectedFlat: flats[0],
       flats
     };
   }
 
-  selectedFlats = (index) => {
-    this.setState({ selectedFlats: flats[index] });
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
   }
 
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
